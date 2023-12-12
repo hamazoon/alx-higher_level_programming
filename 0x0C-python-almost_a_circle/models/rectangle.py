b021c0306f698d4b5917ccf35f504c118c9d10bd
hamazoon <hamazoon@gmail.com>
2023-12-11 17:39:31 -0800

    a  class Rectangle that inherits from Base

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""import base class"""
-
+'''Class Rectangle'''
 
 from models.base import Base
 
 
-"""Rectangle child class"""
-
-
 class Rectangle(Base):
-    """Rectangle child class"""
+    '''
+        Defining the Rectangle class
+        Inherits from:
+            Base
+    '''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle attr"""
-        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
