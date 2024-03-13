9120a2b70ed2ca7424e07e06b65cf933ca310259
hamazoon <hamazoon1@gmail.com>
2024-03-13 14:19:22 -0700

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- show all privileges of MYSQL users : 'user_0d_1' and 'user_0d_1' on 'localhost' server
+-- lists all privileges of the MySQL users user_0d_1 and user_0d_2 
+-- cat 0-privileges.sql | sudo mysql -hlocalhost -uroot -p
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
